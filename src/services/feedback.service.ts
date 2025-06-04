eeb013558aa4f593ec97f61c64e0dc15f0f3cd50
nitishymtpl <108659621+nitishymtpl@users.noreply.github.com>
2025-06-04 19:11:54 +0900

    Update feedback.service.ts

--- src/services/feedback.service.ts
+++ src/services/feedback.service.ts
@@ -1,5 +1,7 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { FeedbackData } from "@/types/response";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClientComponentClient();
 
