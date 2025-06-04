9ca9556bb85c62cbbd066019c1c71ded21f737c1
nitishymtpl <108659621+nitishymtpl@users.noreply.github.com>
2025-06-04 19:11:30 +0900

    Update clients.service.ts

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -1,4 +1,7 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+// Fallback for build time
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClientComponentClient();
 
